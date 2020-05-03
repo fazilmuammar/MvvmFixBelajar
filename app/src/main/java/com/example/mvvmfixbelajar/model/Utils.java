5986048703018bea1a1f9fc00aaaa97efe35cb9e
Fazil Muammar <fazilmuammar007@gmail.com>
2020-05-03 11:15:04 +0700

    First Commit

---
+++ app/src/main/java/com/example/mvvmfixbelajar/model/Utils.java
@@ -0,0 +1,31 @@
+package com.example.mvvmfixbelajar.model;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.mvvmfixbelajar.R;
+
+public class Utils {
+
+    public static void loadImage(ImageView view, String url, CircularProgressDrawable progressDrawable){
+        RequestOptions options = new RequestOptions()
+                .placeholder(progressDrawable)
+                .error(R.mipmap.ic_launcher_round);
+        Glide.with(view.getContext())
+                .setDefaultRequestOptions(options)
+                .load(url)
+                .into(view);
+    }
+
+    public static CircularProgressDrawable getProgressDrawable(Context context){
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(context);
+        circularProgressDrawable.setStrokeWidth(10f);
+        circularProgressDrawable.setCenterRadius(50f);
+        circularProgressDrawable.start();
+        return circularProgressDrawable;
+    }
+}
--- app/src/main/java/com/example/mvvmfixbelajar/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/mvvmfixbelajar/viewmodel/ListViewModel.java
@@ -3,12 +3,18 @@ package com.example.mvvmfixbelajar.viewmodel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.mvvmfixbelajar.model.CountriesService;
 import com.example.mvvmfixbelajar.model.CountryModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class ListViewModel extends ViewModel {
+
     public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
     public MutableLiveData<Boolean> countriesLoadError = new MutableLiveData<Boolean>();
     public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
@@ -17,7 +23,37 @@ public class ListViewModel extends ViewModel {
         fetchCountries();
     }
 
+    private CountriesService countriesService = CountriesService.getInstance();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+
+
+
     public void fetchCountries(){
+
+        loading.setValue(true);
+        disposable.add(
+                countriesService.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<CountryModel>>() {
+                    @Override
+                    public void onSuccess(List<CountryModel> countryModels) {
+                        countries.setValue(countryModels);
+                        countriesLoadError.setValue(false);
+                        loading.setValue(false);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        countriesLoadError.setValue(true);
+                        loading.setValue(false);
+                        e.printStackTrace();
+                    }
+                })
+        );
+
+        /*Dummy Data
         CountryModel country1 = new CountryModel("Indonesia","Jakarta","");
         CountryModel country2 = new CountryModel("Japan","Tokyo","");
         CountryModel country3 = new CountryModel("Malaysia","Kuala Lumpur","");
@@ -30,5 +66,15 @@ public class ListViewModel extends ViewModel {
         countries.setValue(list);
         countriesLoadError.setValue(false);
         loading.setValue(false);
+         */
+
+
+
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
     }
 }
